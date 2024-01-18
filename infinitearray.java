4770e5a79b69a50e7ff22e102be160a258f64c34
menaceJoker <akashrautelacms@gmail.com>
2024-01-18 14:32:31 +0530

    infinite

--- infinitearray.java
+++ infinitearray.java
@@ -1,11 +1,10 @@
 public class infinitearray {
     public static void main(String[] args) {
         int[] ar={1,5,8,14,20,24,26,28,32,36,38,41,43,49,51,55,57,61,68,72,79,85,89,94,98};
-        int target=5;
+        int target=98;
         int ans=search(ar, target);
         System.out.println(ans);
     }
-
     public static int search(int[] ar, int target){
         int start=0;
         int end=2;
@@ -23,7 +22,6 @@ public class infinitearray {
                 end =ar.length-1;
             }
         }
-
         while(start<=end){
             int mid= start+ (end-start) /2;
             if(ar[mid]==target){
